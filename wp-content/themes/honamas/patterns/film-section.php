b1b24950c6baa4222c94e8e2f44859cc7b001adf
Julia Emmerling <julia@MacBook-Air-753.local>
2026-07-29 23:57:45 +0200

    Embed feature film across staging experience

--- wp-content/themes/honamas/patterns/film-section.php
+++ wp-content/themes/honamas/patterns/film-section.php
@@ -4,14 +4,13 @@
  * Slug: honamas/film-section
  * Categories: honamas-sections
  */
-$film_video = get_page_by_path( '39-honamas', OBJECT, 'attachment' );
-$film_video_url = $film_video ? wp_get_attachment_url( $film_video->ID ) : '';
+$youtube_video_id = '8ia99ri8mek';
 ?>
 <!-- wp:group {"align":"full","className":"honamas-film-teaser","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"backgroundColor":"honamas-black","textColor":"honamas-off-white","layout":{"type":"constrained"}} -->
 <div id="film" class="wp-block-group alignfull honamas-film-teaser has-honamas-off-white-color has-honamas-black-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)">
-	<?php if ( $film_video_url ) : ?>
-		<video aria-hidden="true" autoplay class="honamas-film-teaser__video" loop muted playsinline preload="metadata" src="<?php echo esc_url( $film_video_url ); ?>"></video>
-	<?php endif; ?>
+	<div aria-hidden="true" class="honamas-film-teaser__video">
+		<iframe allow="autoplay; encrypted-media; picture-in-picture" allowfullscreen="false" frameborder="0" src="https://www.youtube-nocookie.com/embed/<?php echo esc_attr( $youtube_video_id ); ?>?autoplay=1&amp;mute=1&amp;controls=0&amp;disablekb=1&amp;fs=0&amp;loop=1&amp;modestbranding=1&amp;playlist=<?php echo esc_attr( $youtube_video_id ); ?>&amp;playsinline=1&amp;rel=0" tabindex="-1" title=""></iframe>
+	</div>
 	<!-- wp:group {"align":"wide","layout":{"type":"constrained","justifyContent":"left"}} -->
 	<div class="wp-block-group alignwide">
 		<!-- wp:paragraph {"className":"honamas-kicker","textColor":"honamas-pink-soft"} -->
